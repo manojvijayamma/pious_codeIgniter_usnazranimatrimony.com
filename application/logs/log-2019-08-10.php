c8980d7479d36e589080a4e500a66fb75dbb6d9b
manojvijayan <manojvijayanaluva@gmail.com>
2019-08-10 01:36:50 +0000

    log

---
+++ application/logs/log-2019-08-10.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-08-10 06:22:30 --> Severity: Notice --> Trying to get property 'status' of non-object /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 767
+ERROR - 2019-08-10 06:22:35 --> Severity: Notice --> Trying to get property 'status' of non-object /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 767
