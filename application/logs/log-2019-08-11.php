5be7511a882e84dc6c8f910113400e14e663dc45
manojvijayan <manojvijayanaluva@gmail.com>
2019-08-11 17:06:30 +0000

    index

--- application/logs/log-2019-08-11.php
+++ application/logs/log-2019-08-11.php
@@ -2,3 +2,11 @@
 
 ERROR - 2019-08-11 08:08:54 --> Severity: Notice --> Trying to get property 'status' of non-object /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 767
 ERROR - 2019-08-11 08:08:59 --> Severity: Notice --> Trying to get property 'status' of non-object /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 767
+ERROR - 2019-08-11 19:13:56 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 21:12:35 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 21:12:44 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 21:13:12 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 21:21:29 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 22:33:47 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 22:35:11 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
+ERROR - 2019-08-11 22:36:02 --> Severity: error --> Exception: syntax error, unexpected '===' (T_IS_IDENTICAL), expecting end of file /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/user_profile.php 1252
