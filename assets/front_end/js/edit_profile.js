eb47b60b82e3f281f78a0e862ce45532181c0ae1
manojvijayan <manojvijayanaluva@gmail.com>
2019-08-11 13:11:30 -0400

    p

--- assets/front_end/js/edit_profile.js
+++ assets/front_end/js/edit_profile.js
@@ -2,11 +2,11 @@ var marital_status = $("[name='marital_status']:checked").val();
 if(marital_status !='' && marital_status !='Unmarried')
 {
     $('#total_children').parent().parent().show();
-    $('input[name="status_children"]').parent().parent().show();
+    $('input[name="status_children"]').parent().parent().parent().show();
 }
 else{
     $('#total_children').parent().parent().hide();
-		$('input[name="status_children"]').parent().parent().hide();
+	$('input[name="status_children"]').parent().parent().parent().hide();
 }
 
 
@@ -16,10 +16,10 @@ function showChildern(){
 	if(marital_status !='' && marital_status !='Unmarried')
 	{
 		$('#total_children').parent().parent().show();
-		$('input[name="status_children"]').parent().parent().show();
+		$('input[name="status_children"]').parent().parent().parent().show();
 	}
 	else{
 		$('#total_children').parent().parent().hide();
-		$('input[name="status_children"]').parent().parent().hide();
+		$('input[name="status_children"]').parent().parent().parent().hide();
 	}
 }
