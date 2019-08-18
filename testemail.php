a84a32b6fb5f301e02b68611713bf3c2b3e6dd85
manojvijayan <manojvijayanaluva@gmail.com>
2019-08-18 15:48:51 +0000

    email

--- testemail.php
+++ testemail.php
@@ -4,11 +4,14 @@ $recipient = 'manojvijayanaluva@gmail.com';
 
 $subject = "php mail test";
 $message = "php test message";
-$headers = 'From:' . $sender;
+$headers = 'From:' . $sender.PHP_EOL;
+$headers .= "CC: pious.pious@gmail.com".PHP_EOL;
 
 if (mail($recipient, $subject, $message, $headers))
 {
     echo "Message accepted";
+    
+    
 }
 else
 {
