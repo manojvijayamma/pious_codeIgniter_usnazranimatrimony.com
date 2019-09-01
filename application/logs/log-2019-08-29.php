3c0115e1cb1a4b28178497e4322494937f465605
manojvijayan <manojvijayanaluva@gmail.com>
2019-09-01 07:31:56 +0000

    images

---
+++ application/logs/log-2019-08-29.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-08-29 16:50:32 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:32 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:33 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:33 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:33 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:33 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:34 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:34 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:34 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:34 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:35 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:35 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:35 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:35 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:36 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:36 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:36 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:36 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:37 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:37 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:37 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:37 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
+ERROR - 2019-08-29 16:50:38 --> Severity: Notice --> fwrite(): send of 5 bytes failed with errno=32 Broken pipe /home/smccubnr/public_html/usnazranimatrimony.com/system/libraries/Email.php 2272
