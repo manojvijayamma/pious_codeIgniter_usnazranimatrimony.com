7a8a8925f4d28dd85e1080d0b91d4dd78bdf242e
manojvijayan <manojvijayanaluva@gmail.com>
2019-10-26 16:22:36 +0000

    log

---
+++ application/logs/log-2019-10-17.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-17 03:21:15 --> Severity: error --> Exception: Class 'Aws\Sns\SnsClient' not found /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 740
