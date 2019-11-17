b0e0461a6bbad5a43a881a09678c7c1d3f0f331a
manojvijayan <manojvijayanaluva@gmail.com>
2019-11-17 12:07:16 +0000

    error

--- application/logs/log-2019-11-17.php
+++ application/logs/log-2019-11-17.php
@@ -5,3 +5,4 @@ ERROR - 2019-11-17 11:24:27 --> Severity: error --> Exception: Class 'Aws\Sns\Sn
 ERROR - 2019-11-17 11:28:35 --> Severity: error --> Exception: Class 'Aws\Sns\SnsClient' not found /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 740
 ERROR - 2019-11-17 11:30:46 --> Severity: error --> Exception: Class 'Aws\Sns\SnsClient' not found /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 740
 ERROR - 2019-11-17 12:14:38 --> Severity: error --> Exception: Class 'Aws\Sns\SnsClient' not found /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 740
+ERROR - 2019-11-17 17:06:25 --> Severity: error --> Exception: Class 'Aws\Sns\SnsClient' not found /home/smccubnr/public_html/usnazranimatrimony.com/application/models/Common_model.php 740
