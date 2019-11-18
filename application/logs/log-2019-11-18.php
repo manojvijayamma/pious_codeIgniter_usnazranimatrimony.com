7b872ee7f0d902e6a2eb19bede1ba787b47285d6
manojvijayan <manojvijayanaluva@gmail.com>
2019-11-18 00:39:56 +0000

    photos

---
+++ application/logs/log-2019-11-18.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-11-18 03:39:29 --> Severity: Notice --> Undefined variable: cms_name /home/smccubnr/public_html/usnazranimatrimony.com/application/controllers/Cms.php 102
+ERROR - 2019-11-18 03:41:13 --> Severity: Warning --> Illegal string offset 'matri_id' /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/my_profile_sidebar.php 71
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Uninitialized string offset: 0 /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/my_profile_sidebar.php 71
+ERROR - 2019-11-18 03:41:13 --> Severity: Warning --> Illegal string offset 'photo1' /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/my_profile_sidebar.php 121
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Uninitialized string offset: 0 /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/my_profile_sidebar.php 121
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Undefined variable: plan_expired /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/my_profile_sidebar.php 234
+ERROR - 2019-11-18 03:41:13 --> Severity: Warning --> Illegal string offset 'matri_id' /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/mobile_my_profile_sidebar.php 71
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Uninitialized string offset: 0 /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/mobile_my_profile_sidebar.php 71
+ERROR - 2019-11-18 03:41:13 --> Severity: Warning --> Illegal string offset 'photo1' /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/mobile_my_profile_sidebar.php 121
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Uninitialized string offset: 0 /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/mobile_my_profile_sidebar.php 121
+ERROR - 2019-11-18 03:41:13 --> Severity: Notice --> Undefined variable: plan_expired /home/smccubnr/public_html/usnazranimatrimony.com/application/views/front_end/mobile_my_profile_sidebar.php 232
