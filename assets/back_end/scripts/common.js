dce748933bdf28cb329192de24cf0b94674b6706
=manoj <manojvijayanaluva@gmail.com>
2019-11-23 10:25:14 -0500

    admin

--- assets/back_end/scripts/common.js
+++ assets/back_end/scripts/common.js
@@ -1914,3 +1914,24 @@ function month_year_change()
 	}
 	return false;
 }
+
+
+/* manoj */
+
+$('#total_children').parent().parent().hide();
+$('input[name="status_children"]').parent().parent().hide();
+
+
+
+function showChildern(){
+    var marital_status = $("[name='marital_status']:checked").val();
+	if(marital_status !='' && marital_status !='Unmarried')
+	{
+        $('#total_children').parent().parent().show();
+		$('input[name="status_children"]').parent().parent().show();
+    }
+    else{
+        $('#total_children').parent().parent().hide();
+		$('input[name="status_children"]').parent().parent().hide();
+    }
+}
