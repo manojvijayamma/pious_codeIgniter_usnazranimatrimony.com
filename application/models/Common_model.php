0abc3eb293f30e2f36668508b0af9872d94d5785
Manoj Vijayan <BEO\manoj.vijayan@act18.beo.in>
2019-12-02 09:42:19 +0530

    any

--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -1396,8 +1396,9 @@ class Common_model extends CI_Model {
 						<select '.$is_multi.' '.$onChange.' '.$is_required.' name="'.$key.$is_multi_par.'" id="'.$key.'" class="form-control '.$class.' " '.$extra.' >';
 							if($display_placeholder =='Yes')
 							{
-								$return_content.='<option selected value="" >Any</option>';
+								
 							}
+							$return_content.='<option selected value="" >Any</option>';
 							
 						if(isset($value_arr) && $value_arr !='' && count($value_arr) > 0)
 						{
